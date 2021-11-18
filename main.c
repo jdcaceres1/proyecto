2d78f903c3524a3fb614193822827b56256b6143
jdcaceres1 <joscaceres78@gmail.com>
2021-11-17 23:26:15 -0600

    main.c actualizado

--- main.c
+++ main.c
@@ -3,35 +3,65 @@
 #include <stdlib.h>
 #include <time.h>
 
-char jugador1, jugador2;
-int main(int argc, char const *argv[]) {
-   printf("Escribe el nombre del jugador1: ");
-   scanf("%c", &jugador1);
+#define TRUE 1
+#define FALSE 0
 
-    printf("Escribe el nombre del jugador2: ");
-    scanf("%c", &jugador2);
-
-int main();
-    srand(time(NULL));
-
-    int matriz [9][9];
-    int i, j;
-
-    for (int i = 0; i < 9; i++){
-        for (int j = 0; j < 9; j++){
-            matriz[i][j] = 1 + rand() % 9;
-        }
-        
-    }
-
-    for (int i = 0; i < 9; i++){
-        for (int j = 0; j < 9; j++){
-            printf("%d\t ",matriz[i][j]);
-            
+int main(){
     
-    return 0; 
-        }
-    }
+    srand(time(NULL));
+    int matriz[9][9];
+    int matrizb[9][9];
+    int opcion;
+    char repetir = TRUE;
+    
+    do {
+        
+        // Texto del menú
+        printf("Tablero\n");
+        printf("\n\nMenu de Opciones:\n\n");
+        printf("1. Jugar\n");
+        printf("2. Puntuacion\n");
+        printf("3. Intrucciones\n");
+        printf("0. Salir\n");
+        
+        printf("\nSeleccione una opcion:");
+        scanf("%d", &opcion);
+        
+        switch (opcion) {
+            case 1: 
+            printf("\n\nusted ha seleccionado la opcion:Jugar\n");
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    matriz[i][j] = rand() % (9 + 1 - 1) + 1;
+                    printf("|%d|", matriz[i][j]);
+                    
+                }
+                printf("\n");
+            }
+            
+                break;
+                
+            case 2:
+                printf("Instrucciones de la opcion 2\n");
+                system("pause>nul"); // Pausa
+                break;
+                
+            case 3:
+                printf("Instrucciones de la opcion 3\n");
+                system("pause>nul"); // Pausa            
+                break;
+            
+            case 0:
+            	repetir = FALSE;
+            	break;
+        }        
+    } while (repetir);
+	 
+    return 0;
+}
+    
 
 
 
